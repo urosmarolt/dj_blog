90b04768a264e288f6797cc848d72c9a8ce8d191
urosmarolt <uros.marolt@gmail.com>
2016-06-06 09:41:05 +0200

    Added reviews to the sitemap

--- blog/sitemaps.py
+++ blog/sitemaps.py
@@ -1,5 +1,5 @@
 from django.contrib.sitemaps import Sitemap
-from .models import Post
+from .models import Post, Review
 
 class PostSitemap(Sitemap):
     changefreq = 'weekly'
@@ -7,6 +7,16 @@ class PostSitemap(Sitemap):
 
     def items(self):
         return Post.published.all()
+
+    def lastmod(self, obj):
+        return obj.publish
+
+class ReviewSitemap(Sitemap):
+    changefreq = 'weekly'
+    priority = 0.8
+
+    def items(self):
+        return Review.published.all()
 
     def lastmod(self, obj):
         return obj.publish
--- django_app/urls.py
+++ django_app/urls.py
@@ -18,10 +18,11 @@ from django.conf.urls.static import static
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
-from blog.sitemaps import PostSitemap
+from blog.sitemaps import PostSitemap, ReviewSitemap
 
 sitemaps = {
     'posts': PostSitemap,
+    'reviews': ReviewSitemap,
 }
 
 
